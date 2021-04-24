e36fd543c4d26bf9aed63d26a01601c7de44cd6f
hasantunahan <hasantunahan@niltekyazilim.com>
2021-04-24 21:57:51 +0300

    only login email password

--- models/userModel.js
+++ models/userModel.js
@@ -1,7 +1,9 @@
 const mongoose = require("mongoose");
-
+const createError = require("http-errors");
 const Schema = mongoose.Schema;
+const bcrypt = require("bcrypt");
 
+const Joi = require("joi");
 const userSchema = new Schema(
   {
     name: {
@@ -34,6 +36,53 @@ const userSchema = new Schema(
   },
   { collection: "users", timestamps: true }
 );
+
+const schema = Joi.object({
+  name: Joi.string().min(3).max(50).trim(),
+  username: Joi.string().min(3).max(50).trim(),
+  email: Joi.string().trim().email(),
+  password: Joi.string().trim(),
+});
+
+userSchema.methods.joiValidation = function (userObject) {
+  schema.required();
+  return schema.validate(userObject);
+};
+
+userSchema.statics.joiValidationForUpdate = function (userObject) {
+  return schema.validate(userObject);
+};
+
+userSchema.methods.toJSON = function () {
+  const user = this.toObject();
+  delete user._id;
+  delete user.createdAt;
+  delete user.updatedAt;
+  delete user.__v;
+  delete user.password;
+
+  return user;
+};
+
+userSchema.statics.login = async function (email, password) {
+  const { error, value } = schema.validate({ email, password });
+
+  if (error) {
+    throw createError(400, error);
+  }
+
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw createError(400, "login is failed : email or password incorrect");
+  }
+  const pass = await bcrypt.compare(password, user.password);
+
+  if (!pass) {
+    throw createError(400, "login is failed : email or password incorrect");
+  }
+
+  return user;
+};
 
 const User = mongoose.model("User", userSchema);
 
--- router/user_router.js
+++ router/user_router.js
@@ -1,45 +1,62 @@
 const router = require("express").Router();
 const User = require("../models/userModel");
-const createError = require('http-errors');
+const createError = require("http-errors");
+const bcrypt = require("bcrypt");
 
 router.get("/", async (req, res) => {
   const allUser = await User.find({});
   res.json(allUser);
 });
 
-router.get("/:id",async (req, res, next) => {
+router.get("/:id", async (req, res, next) => {
   try {
-    const findUser = await User.findOne({})
+    const findUser = await User.findOne({});
   } catch (e) {
-    next(createError(400,e));
+    next(createError(400, e));
   }
 });
 
 router.post("/", async (req, res, next) => {
   try {
     const addUser = new User(req.body);
-    const result = await addUser.save();
-    res.status(200).json(result);
+    addUser.password = await bcrypt.hash(addUser.password, 10);
+    const { error, value } = addUser.joiValidation(req.body);
+
+    if (error) {
+      next(createError(400, error));
+    } else {
+      const result = await addUser.save();
+      res.status(200).json(result);
+    }
   } catch (e) {
-    next(createError(400,e));
+    next(createError(400, e));
   }
 });
 
 router.patch("/:id", async (req, res, next) => {
+  const { error, value } = User.joiValidationForUpdate(req.body);
   const id = req.params.id;
-  try {
-    const result = await User.findByIdAndUpdate(id, req.body, {
-      new: true,
-      runValidators: true,
-    });
-    if (result) {
-      return res.json(result);
-    } else {
-        throw createError(404,`${req.params.id} user not found`);
 
+  if (req.body.hasOwnProperty("password")) {
+    req.body.password = await bcrypt.hash(req.body.password, 10);
+  }
+
+  if (error) {
+    next(createError(400, error));
+  } else {
+    try {
+      const result = await User.findByIdAndUpdate(id, req.body, {
+        new: true,
+        runValidators: true,
+      });
+      if (result) {
+        return res.json(result);
+      } else {
+        throw createError(404, `${req.params.id} user not found`);
+      }
+    } catch (e) {
+      next(createError(400, e));
     }
-  } catch (e) {
-    next(createError(400,e));
   }
 });
 
@@ -49,10 +66,19 @@ router.delete("/:id", async (req, res, next) => {
     if (result) {
       return res.json(`${req.params.id} users deleted successfully`);
     } else {
-      throw createError(404,`${req.params.id} user not found`);
+      throw createError(404, `${req.params.id} user not found`);
     }
   } catch (e) {
-    next(createError(400,e));
+    next(createError(400, e));
+  }
+});
+
+router.post("/login", async (req, res, next) => {
+  try {
+    const user = await User.login(req.body.email, req.body.password);
+    res.json(user);
+  } catch (e) {
+    next(e);
   }
 });
 
